b4fb5e30ef5424f17c6fb636b7b5df361195cccc
Ari Suutari <ari@stonepile.fi>
2013-12-18 17:33:47 +0200

    Add support for Energy Micro RTC scheduling clock instead of standard SysTick. This allows energy savings - using SysTick won't allow very deep sleep.

---
+++ ports/cortex-m/clock_efm32_rtc.c
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2011-2013, Ari Suutari <ari@stonepile.fi>.
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote
+ *     products derived from this software without specific prior written
+ *     permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <picoos.h>
+#include <string.h>
+
+#include "em_device.h"
+#include "em_chip.h"
+#include "em_rtc.h"
+#include "em_cmu.h"
+
+#if PORTCFG_CLOCK_EFM32_RTC == 1
+
+/*
+ * Initialize Systick timer.
+ */
+
+void portInitClock(void)
+{
+  /* Starting LFRCO and waiting until it is stable */
+  CMU_OscillatorEnable(cmuOsc_LFRCO, true, true);
+#warning PORTCFG_LFXO_HZ
+  /* Routing the LFRCO clock to the RTC */
+  CMU_ClockSelectSet(cmuClock_LFA,cmuSelect_LFRCO);
+  CMU_ClockEnable(cmuClock_RTC, true);
+
+  /* Enabling clock to the interface of the low energy modules */
+  CMU_ClockEnable(cmuClock_CORELE, true);
+
+  RTC_Init_TypeDef rtcInit = RTC_INIT_DEFAULT;
+
+  rtcInit.enable   = true;      /* Enable RTC after init has run */
+  rtcInit.comp0Top = true;      /* Clear counter on compare match */
+  rtcInit.debugRun = false;     /* Counter shall keep running during debug halt. */
+
+#define LFRCO_FREQUENCY              32768
+
+  /* Setting the compare value of the RTC */
+  RTC_CompareSet(0, (LFRCO_FREQUENCY / HZ) - 1);
+
+  /* Enabling Interrupt from RTC */
+  RTC_IntEnable(RTC_IFC_COMP0);
+  NVIC_EnableIRQ(RTC_IRQn);
+  NVIC_SetPriority(RTC_IRQn, PORT_SYSTICK_PRI);
+
+  /* Initialize the RTC */
+  RTC_Init(&rtcInit);
+
+}
+
+/*
+ * Timer interrupt from SysTick.
+ */
+void RTC_IRQHandler(void)
+{
+  c_pos_intEnter();
+  RTC_IntClear(RTC_IFC_COMP0);
+  c_pos_timerInterrupt();
+  c_pos_intExitQuick();
+}
+
+#endif
