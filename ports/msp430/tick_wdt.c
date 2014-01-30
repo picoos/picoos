2dabfc469c2b299ec3b9129de56791e4861b107e
Ari Suutari <ari@stonepile.fi>
2014-01-30 17:24:35 +0200

    Add watchdog timer based tick initial implementation.

---
+++ ports/msp430/tick_wdt.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2011-2014, Ari Suutari <ari@stonepile.fi>.
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote
+ *     products derived from this software without specific prior written
+ *     permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define NANOINTERNAL
+#include <picoos.h>
+#include <msp430.h>
+#include <in430.h>
+#include <string.h>
+
+#if PORTCFG_TICK_WDT == 1
+
+void timerIrqHandler(void);
+
+#ifdef HZ != 512
+#error HZ must be 512 for WDT timer
+#endif
+
+#if !defined(PORTCFG_XT1_HZ) || PORTCFG_XT1_HZ == 0
+#error need crystal for now
+#endif
+
+/*
+ * Initialize timer.
+ */
+
+void portInitTimer(void)
+{
+  WDTCTL = WDTPW + WDTTMSEL + WDTCNTCL + WDTSSEL + WDTIS1 + WDTIS0;
+  IE1 |= WDTIE;
+}
+
+void PORT_NAKED __attribute__((interrupt(WDT_VECTOR))) timerIrqHandler()
+{
+  portSaveContext();
+  c_pos_intEnter();
+  c_pos_timerInterrupt();
+  c_pos_intExit();
+  portRestoreContext();
+}
+
+#endif
