313bf447625dab575ba20d9aae5f8ea9bba56977
Ari Suutari <ari@stonepile.fi>
2015-04-06 20:21:34 +0300

    Add support for Texas Instruments MSP432 chip.

---
+++ ports/cortex-m/msp432/con_eusci.c
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2015, Ari Suutari <ari@stonepile.fi>.
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote
+ *     products derived from this software without specific prior written
+ *     permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define NANOINTERNAL
+#include <picoos.h>
+#include <stdbool.h>
+
+#include "driverlib.h"
+
+/*
+ * Initialize MSP432 eusci module.
+ */
+
+#if PORTCFG_CON_EUSCI > 0
+
+#define PORT EUSCI_A0
+#define IRQ EUSCIA0_IRQn
+
+#if NOSCFG_FEATURE_CONOUT == 1 || NOSCFG_FEATURE_CONIN == 1
+
+void portInitConsole(void)
+{
+  PORT->rCTLW0.r = UCSWRST;
+  PORT->rCTLW0.b.bSSEL = 2; // SMCLK
+
+  float n = CS_getSMCLK() / 115200;
+  uint16_t div;
+  uint16_t frac;
+
+  div = n / 16;
+  frac = ((n / 16) - div) * 16;
+
+  PORT->rBRW = div;
+  PORT->rMCTLW.b.bBRF = frac;
+  PORT->rMCTLW.b.bOS16 = 1;
+
+  PORT->rCTLW0.b.bSWRST = 0;
+
+  // Console shouldn't be realtime-critical,
+  // use low interrupt priority for it.
+  NVIC_ClearPendingIRQ(EUSCIA0_IRQn);
+  NVIC_SetPriority(EUSCIA0_IRQn, PORT_PENDSV_PRI - 1);
+  NVIC_EnableIRQ(EUSCIA0_IRQn);
+
+#if NOSCFG_FEATURE_CONIN == 1
+
+  PORT->rIE.b.bRXIE = 1; // Enable RX interrupt.
+
+#endif
+}
+
+/*
+ * Usart interrupt handler.
+ */
+
+void EUSCIA0_Handler()
+{
+  c_pos_intEnter();
+
+
+  switch (PORT->rIV) {
+  case 2:
+#if NOSCFG_FEATURE_CONIN == 1
+
+    {
+      uint8_t ch;
+      ch = PORT->rRXBUF;
+      c_nos_keyinput(ch);
+    }
+
+#endif
+   break;
+
+  case 4:
+    PORT->rIE.b.bTXIE = 0; // Disable TX interrupt
+#if NOSCFG_CONOUT_HANDSHAKE == 1 && NOSCFG_FEATURE_CONOUT == 1
+    c_nos_putcharReady();
+#endif
+    break;
+
+  default:
+    break;
+  }
+
+  c_pos_intExitQuick();
+}
+
+#if NOSCFG_FEATURE_CONOUT == 1
+/*
+ * Console output.
+ */
+
+UVAR_t
+p_putchar(char c)
+{
+#if NOSCFG_CONOUT_HANDSHAKE != 0
+
+  if (PORT->rIE.b.bTXIE) // If interrupt is enabled, previous char is not yet transmitted.
+    return 0;
+
+  PORT->rTXBUF.b.bTXBUF = c;
+  PORT->rIE.b.bTXIE = 1; // Enable TX interrupt
+
+#else
+
+  while (!(PORT->rIFG.b.bTXIFG)); // Wait for TX buffer to empty
+  PORT->rTXBUF.b.bTXBUF = c;
+
+#endif
+
+  return 1;
+}
+#endif
+
+#endif
+#endif
---
+++ ports/cortex-m/msp432/tick_wdt.c
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2015, Ari Suutari <ari@stonepile.fi>.
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote
+ *     products derived from this software without specific prior written
+ *     permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT,  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define NANOINTERNAL
+
+#include <picoos.h>
+#include "driverlib.h"
+
+#if PORTCFG_TICK_WDT == 1
+
+/*
+ * Initialize wdt as tick source.
+ */
+
+void portInitClock(void)
+{
+  uint16_t div;
+
+  switch (CS_getBCLK() / HZ) {
+  case 32768:
+    div = WDT_A_CLOCKITERATIONS_32K;
+    break;
+
+  case 8192:
+    div = WDT_A_CLOCKITERATIONS_8192;
+    break;
+
+  case 512:
+    div = WDT_A_CLOCKITERATIONS_512;
+    break;
+
+  case 64:
+    div = WDT_A_CLOCKITERATIONS_64;
+    break;
+
+  default:
+    P_ASSERT("WDT cannot produce requested HZ", false);
+    break;
+  }
+
+  MAP_WDT_A_initIntervalTimer(WDT_A_CLOCKSOURCE_XCLK, div);
+
+  NVIC_EnableIRQ(WDT_A_IRQn);
+  NVIC_SetPriority(WDT_A_IRQn, PORT_SYSTICK_PRI);
+
+  MAP_WDT_A_startTimer();
+}
+
+/*
+ * Timer interrupt from WDT.
+ */
+void WDT_Handler(void)
+{
+  c_pos_intEnter();
+  c_pos_timerInterrupt();
+  c_pos_intExitQuick();
+}
+
+#endif
