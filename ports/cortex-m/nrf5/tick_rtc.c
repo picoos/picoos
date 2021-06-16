e5f46a6047c2e17e29d3fe57db6d2d7830e59188
Ari Suutari <ari@stonepile.fi>
2021-06-16 19:06:30 +0300

    Fix nrf52 sleep time calculation.
    There is something odd going on with nrf52832 RTC. When
    NRF_RTC_TASK_CLEAR is issued in p_pos_powerTickSuspend, it
    doesn't always take effect and old value is left in RTC counter.
    This results in incorrect sleep time calculation when processor
    wakes up. Clearing the counter after wakeup seems to fix the problem.

--- ports/cortex-m/nrf5/tick_rtc.c
+++ ports/cortex-m/nrf5/tick_rtc.c
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2016, Ari Suutari <ari@stonepile.fi>.
- * All rights reserved. 
- * 
+ * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  *  1. Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
  *  2. Redistributions in binary form must reproduce the above copyright
@@ -13,8 +13,8 @@
  *     documentation and/or other materials provided with the distribution.
  *  3. The name of the author may not be used to endorse or promote
  *     products derived from this software without specific prior written
- *     permission. 
- * 
+ *     permission.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -35,12 +35,13 @@
 
 void portInitClock(void)
 {
+  nrf_rtc_task_trigger(NRF_RTC1, NRF_RTC_TASK_STOP);
   nrf_rtc_prescaler_set(NRF_RTC1, RTC_FREQ_TO_PRESCALER(HZ));
   nrf_rtc_event_enable(NRF_RTC1, NRF_RTC_EVENT_TICK);
   nrf_rtc_int_enable(NRF_RTC1, NRF_RTC_INT_TICK_MASK);
 
   NVIC_SetPriority(RTC1_IRQn, PORT_SYSTICK_PRI);
-  NVIC_EnableIRQ(RTC1_IRQn); 
+  NVIC_EnableIRQ(RTC1_IRQn);
 
   nrf_rtc_task_trigger(NRF_RTC1, NRF_RTC_TASK_START);
 }
@@ -93,6 +94,7 @@ void p_pos_powerTickResume()
  * Now step the timer with amount of ticks we really slept.
  */
   c_pos_timerStep(nrf_rtc_counter_get(NRF_RTC1));
+  nrf_rtc_task_trigger(NRF_RTC1, NRF_RTC_TASK_CLEAR);
 
 /*
  * Safe to enable tick now.
